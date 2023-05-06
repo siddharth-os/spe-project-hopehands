e9976a8ccc2a431aeeba1820ccd70d22a92d16ae
siddharth-os <siddharth.gupta4244@gmail.com>
2023-05-06 15:03:43 +0530

    webook 4

--- src/component/userHome.jsx
+++ src/component/userHome.jsx
@@ -33,7 +33,7 @@ export default function UserHome() {
       else{
         danger();
         navigate("/");
-        console.log("hello");
+        // console.log("hello");
       }
     },[])
   if(!isUser()){
